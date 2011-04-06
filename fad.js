a35775775c46c8d6ec14a2a832c74a39272dd787
pifantastic <aaron.forsander@gmail.com>
2011-04-05 19:59:23 -0500

    Sweeping changes.  No more fadframe.html.  That might come back to bite me.  Turns out, all of my Safari woes were solved by calling document.close() after inserting the ad code into the igrame.

--- fad.js
+++ fad.js
@@ -10,17 +10,6 @@
  *
  */
 (function( global, doc, undefined ) {
-  // Eventually global fad function.
-  // Call this for a good time.
-  var fad = function ( adList ) {
-    for ( var i = 0, length = adList.length; i < length; i++ ) {
-      boom( adList[i] );
-    }
-  };
-  
-  // Keep track of iframes we create so we can clean them up.
-  var iframes = {};
-  
   // Hot DOM ready implementation.
   var alReady = function( n, t, ready ) {
     ready = function(){ for ( t = 0; ready && t < n; ) ready[ t++ ](); ready = 0 }
@@ -31,70 +20,39 @@
     return function( fn ){ ready ? ready[ n++ ] = fn : fn() }
   }( 0, 1 );
   
-  // Load an ad in an iframe.
-  var boom = function ( ad ) {
-    alReady(function() {
-      // Create an iframe and grab the ad's code.
-      var iframe = doc.createElement( 'iframe' ),
-          container = doc.getElementById( ad.id ),
-          script = container && container.children.length && container.children[0];
-      
-      // Ninja frame.
-      iframe.style.display = 'none';
-      
-      // Send the ad's code to the iframe when it loads.
-      var onload = function() {
-        var content = iframe.contentWindow || iframe.contentDocument.defaultView;
-        if ( script && content.loadAd ) {
-          content.loadAd( ad, script );
-        }
-      };
-      
-      // IE doesn't fire .onload for programatically created iframes.
-      if ( iframe.addEventListener ) {
-        iframe.addEventListener( "load", onload, false );
-      } else if ( iframe.attachEvent ) {
-        iframe.attachEvent( "onload", onload );
-      }
-  		
-      // Add the iframe to the document.
-      iframes[ ad.id ] = iframe;
-      doc.body.appendChild( iframe );
-      
-      iframe.src = fad.options.frameLocation;
-    });
-  };
-  
-  // Let an iframe inject its ad code back into the dom.
-  fad.hollaback = function ( ad, html ) {
-    var element = document.getElementById( ad.id ),
-        iframe;
-    if ( element ) {
-      element.innerHTML = html;
-      // Cleanup iframes.
-      if ( (iframe = iframes[ ad.id ]) && iframe.parentNode ) {
-      	iframe.parentNode.removeChild( iframe );
+  alReady(function() {
+    var fads = document.querySelectorAll
+      ? document.querySelectorAll( "script[type='script/fad']" )
+      : document.getElementsByTagName('script');
+    
+    for ( var i = 0, length = fads.length; i < length; i++ ) {
+      // Make sure we only grab fad blocks in case we couldn't use qSA.
+      if ( fads[i] && ( fads[i].type === 'script/fad' ) ) {
+        var script = fads[i],
+            iframe = doc.createElement( 'iframe' ),
+            div = doc.createElement( 'div' );
+        
+        // Hide iframe and add it to the DOM.
+        iframe.style.display = 'none';
+        doc.body.appendChild( iframe );
+
+        // Shove the add code into the frame and let it 
+        // doc.write its little heart out.
+        var iwin = iframe.contentWindow || iframe.contentDocument.defaultView;
+        iwin.document.write( '<scr' + 'ipt>' + script.innerHTML + '</scr' + 'ipt>' );
+
+        // Needed for Safari.
+        iwin.document.close();
+
+        // Grab the ad output and put it back into the DOM.
+        div.innerHTML = iwin.document.body.innerHTML;
+        div.id = script.id;
+        script.parentNode.insertBefore( div, script );
+
+        // Clean up.  Can we reuse the iframe?
+        // script.parentNode.removeChild( script ); Need to clone document.scripts for this to work in IE
+        iframe.parentNode.removeChild( iframe ); 
       }
     }
-  };
-
-  // Default options
-  fad.options = {
-    frameLocation : '/fadframe.html',
-    forceSingle   : false
-  };
-
-  // This is a shallow extend on the options object
-  fad.configure = function ( options ) {
-    var i;
-
-    // NOTE :: if you intend on supporting browsers that don't have hasOwnProperty, you'll need a shim for it
-    for ( i in options ) {
-      if ( options.hasOwnProperty(i) ) {
-        fad.options[i] = options[i];
-      }
-    }
-  };
-
-  global.fad = fad;
+  });
 })( this, this.document );
